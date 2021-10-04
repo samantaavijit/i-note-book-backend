1675b5db8767319178fd0a9562cb632bfeee2680
Avijit Samanta <samantaavijit.2000@gmail.com>
2021-10-04 20:33:41 +0530

    implement route

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const connectToMongo = require("./config/db");
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 3050
 
 connectToMongo();
+
+// Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
